30456a145e88bba2652596a07124ad50f5b82d4e
Abduvohid <vohid4970@gmail.com>
2024-01-10 12:49:58 +0500

    add 6px translate

--- src/App.tsx
+++ src/App.tsx
@@ -196,7 +196,7 @@ export const App = () => {
       right: "9%",
       position: "fixed",
       zIndex: 999,
-      translate: "-15px 20px",
+      translate: "-15px 6px",
     },
     tocButtonBarTop: {
       ...ReactReaderStyle.tocButtonBarTop,
@@ -241,7 +241,7 @@ export const App = () => {
       left: "none",
       right: "9%",
       position: "fixed",
-      translate: "-15px 20px",
+      translate: "-15px 6px",
     },
     tocButtonBarTop: {
       ...ReactReaderStyle.tocButtonBarTop,
@@ -296,7 +296,7 @@ export const App = () => {
       left: "none",
       right: "9%",
       position: "fixed",
-      translate: "-15px 20px",
+      translate: "-15px 6px",
     },
 
     tocArea: {
@@ -341,7 +341,7 @@ export const App = () => {
       left: "none",
       right: "9%",
       position: "fixed",
-      translate: "-15px 20px",
+      translate: "-15px 6px",
     },
 
     tocArea: {
@@ -386,7 +386,7 @@ export const App = () => {
       left: "none",
       right: "9%",
       position: "fixed",
-      translate: "-15px 20px",
+      translate: "-15px 6px",
     },
 
     tocArea: {
